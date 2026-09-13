45cecdb3e7c68e84f1e4808eaa21a5633b460a35
arik509 <sabirhossainarik34@gmail.com>
2026-09-14 03:54:59 +0600

    add global error handling and response utilities

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,32 @@ import "dotenv/config";
 
 
 export const config = {
+
     port: process.env.PORT || 5000,
 
     database_url: process.env.DATABASE_URL,
+
+
+    jwt: {
+
+        accessSecret:
+            process.env.JWT_ACCESS_SECRET as string,
+
+        refreshSecret:
+            process.env.JWT_REFRESH_SECRET as string,
+
+
+        accessExpiresIn:
+            process.env.JWT_ACCESS_EXPIRES_IN,
+
+
+        refreshExpiresIn:
+            process.env.JWT_REFRESH_EXPIRES_IN
+
+    },
+
+
+    bcryptSaltRounds:
+        Number(process.env.BCRYPT_SALT_ROUNDS)
+
 };
